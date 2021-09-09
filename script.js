410fc193cde17955b8e62309242374b328baab17
Vana-06 <90236177+Vana-06@users.noreply.github.com>
2021-09-09 11:19:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+ function compute() 
+{ 
+    var principle = document.getElementById("principal").value; 
+ if(principle==0 || principle<0){ 
+  if(window.confirm("Enter Positive Number")) 
+   document.getElementById("principal").value = ""; 
+   document.getElementById("principal").focus(); 
+ } 
+ else { 
+  var rate = document.getElementById("rate").value; 
+  var years = document.getElementById("years").value; 
+  var amount = principle * years * rate /100; 
+  var year = new Date().getFullYear()+parseInt(years); 
+  document.getElementById("result").innerHTML="\<br\>\<br\>If you deposit <mark>"+principle+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>\<br\>" 
+ } 
+     
+}  
+ 
+function updateRate()  
+{ 
+    var rateval = document.getElementById("rate").value; 
+    document.getElementById("rate_val").innerText = rateval+"%"; 
 }
-        
